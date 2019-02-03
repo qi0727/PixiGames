216669d3335b1b64d5c38ec524606964d73e5956
Qi <qi2627@gmail.com>
2019-02-03 13:37:38 +0800

    - added fps count

--- src/Main.ts
+++ src/Main.ts
@@ -6,3 +6,13 @@ document.body.appendChild(app.view);
 
 const gameMenu = new GameMenu();
 gameMenu.setUp(app);
+
+const framerate = document.getElementById("framerate");
+
+
+const updateFps = (delta: number) => {
+    if (framerate) {
+        framerate.innerHTML = app.ticker.FPS.toString();
+    }
+}
+app.ticker.add(updateFps);
